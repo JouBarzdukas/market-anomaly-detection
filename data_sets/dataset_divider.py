7fef5fcf002bd4c3cdb2826bc6da91a5cf0709ef
Jou Barzdukas <joubarzdukas@gmail.com>
2025-06-17 01:22:39 -0400

    reformat xlsx data to sql database

--- data_sets/dataset_divider.py
+++ data_sets/dataset_divider.py
@@ -4,6 +4,7 @@
 
 
 import pandas as pd
+from sqlalchemy import create_engine
 #from sklearn.model_selection import KFold
 #from sklearn.model_selection import TimeSeriesSplit
 
@@ -12,31 +13,18 @@ import pandas as pd
 # Data input is going to be .xlsx file for now
 
 
+def initialize_data_pointer(FILE_LOCATION):
+    data_set = pd.read_excel(FILE_LOCATION, skiprows=5, header=0)
+    data_set = data_set.loc[:, ~data_set.columns.str.contains(r"^Unnamed")]
+    data_set = data_set[data_set['Ticker'].notna()].reset_index(drop=True)
+    return data_set
+
+def format_to_sql(data_set, file_location):
+    engine = create_engine(f"sqlite:///{file_location}")
+    # write an auto-incrementing 'id' column as primary key
+    data_set.to_sql('financial_data', con=engine, if_exists='replace', index=True, index_label='id')
+
+
 FILE_LOCATION = "data_sets/FinancialMarketData.xlsx"
-
-def test_open_file():
-    try:
-        df = pd.read_excel(FILE_LOCATION)
-        print("File opened successfully.")
-        return df
-    except Exception as e:
-        print(f"Error opening file: {e}")
-        return None
-    
-
-def return_single_row():
-    import pandas as pd
-
-    df = pd.read_excel(FILE_LOCATION,
-                    skiprows=5,
-                    header=0)
-    df = df[df['Ticker'].notna()].reset_index(drop=True)
-
-    if isinstance(df.columns, pd.MultiIndex):
-        df.columns = df.columns.map(' '.join).str.strip()
-    print(df.iloc[0])
-
-
-    
-test_open_file()
-return_single_row()
+data_set = initialize_data_pointer(FILE_LOCATION)
+format_to_sql(data_set, 'data_sets/all_data.db')
