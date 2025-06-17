8b204c09f4484491a17c598a936a6e7d3ac298a0
Jou Barzdukas <joubarzdukas@gmail.com>
2025-06-17 01:53:58 -0400

    create basic pipeline for database for noise cancelation

--- data_sets/dataset_divider.py
+++ data_sets/dataset_divider.py
@@ -1,17 +1,14 @@
 # Goal | Divide a dataset into training, validation, and test sets
-# Issue | Dataset is time based -> certian time periods may have extra noise so selecticing a random sample may not be ideal
+# Issue | Dataset is time based -> certain time periods may have extra noise so selecting a random sample may not be ideal
 # Fix | Use Walk forward validation AND purged K-Fold cross-validation
 
-
+import os
 import pandas as pd
 from sqlalchemy import create_engine
-#from sklearn.model_selection import KFold
-#from sklearn.model_selection import TimeSeriesSplit
-
+from sklearn.model_selection import TimeSeriesSplit, KFold
 
 # Two methods -> KFold and TimeSeriesSplit
 # Data input is going to be .xlsx file for now
-
 
 def initialize_data_pointer(FILE_LOCATION):
     data_set = pd.read_excel(FILE_LOCATION, skiprows=5, header=0)
@@ -19,12 +16,47 @@ def initialize_data_pointer(FILE_LOCATION):
     data_set = data_set[data_set['Ticker'].notna()].reset_index(drop=True)
     return data_set
 
-def format_to_sql(data_set, file_location):
+def format_to_sql(data_set, file_location, table_name):
     engine = create_engine(f"sqlite:///{file_location}")
-    # write an auto-incrementing 'id' column as primary key
-    data_set.to_sql('financial_data', con=engine, if_exists='replace', index=True, index_label='id')
+    data_set.to_sql(table_name, con=engine, if_exists='replace', index=True, index_label='id')
+    engine.dispose()
+
+def make_training_data(data_set, n_splits=5):
+    kfold = KFold(n_splits=n_splits, shuffle=False)
+    walk_forward = TimeSeriesSplit(n_splits=n_splits)
+    return {
+        'KFold':      [data_set.iloc[train].reset_index(drop=True) for train, _ in kfold.split(data_set)],
+        'WalkForward':[data_set.iloc[train].reset_index(drop=True) for train, _ in walk_forward.split(data_set)]
+    }
+
+def make_validation_data(data_set, n_splits=5):
+    kfold = KFold(n_splits=n_splits, shuffle=False)
+    walk_forward = TimeSeriesSplit(n_splits=n_splits)
+    return {
+        'KFold':      [data_set.iloc[valid].reset_index(drop=True) for _, valid in kfold.split(data_set)],
+        'WalkForward':[data_set.iloc[valid].reset_index(drop=True) for _, valid in walk_forward.split(data_set)]
+    }
+
+def push_to_sql(data_sets, path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    engine = create_engine(f"sqlite:///{path}")
+    if isinstance(data_sets, list):
+        for i, df in enumerate(data_sets, 1):
+            df.to_sql(f"fold_{i}", con=engine, if_exists="replace", index=True, index_label="id")
+    else:
+        data_sets.to_sql("financial_data", con=engine, if_exists="replace", index=True, index_label="id")
+    engine.dispose()
 
 
-FILE_LOCATION = "data_sets/FinancialMarketData.xlsx"
-data_set = initialize_data_pointer(FILE_LOCATION)
-format_to_sql(data_set, 'data_sets/all_data.db')
+file_location    = "data_sets/FinancialMarketData.xlsx"
+data_set         = initialize_data_pointer(file_location)
+format_to_sql(data_set, 'data_sets/all_data.db', 'financial_data')
+
+training_data    = make_training_data(data_set)
+validation_data  = make_validation_data(data_set)
+
+for train_type, data in training_data.items():
+    push_to_sql(data, f"data_sets/{train_type}/{train_type}_formatted.db")
+
+for valid_type, data in validation_data.items():
+    push_to_sql(data, f"data_sets/{valid_type}/{valid_type}_formatted.db")
